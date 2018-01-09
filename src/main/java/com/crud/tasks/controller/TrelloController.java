bdc5910a10ca98de320e1814d33d0dd9257b9379
mzcz <mzcz@wp.pl>
2018-01-09 17:35:01 +0100

    mockito test

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -22,23 +21,26 @@ public class TrelloController {
     private TrelloClient trelloClient;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
-    public void getTrelloBoards() {
+    public List<TrelloBoardDto> getTrelloBoards() {
 
-        List<Optional<TrelloBoardDto>> trelloBoards = trelloClient.getTrelloBoards().stream()
-        .filter(trelloBoard -> trelloBoard.orElse(null).getName().contains("Kodilla"))
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards().stream()
+        .filter(trelloBoard -> trelloBoard.getName().contains("Kodilla"))
                 .collect(Collectors.toList());
 
         trelloBoards.forEach(trelloBoardDto -> {
 
-            System.out.println(trelloBoardDto.get().getName() + " - " + trelloBoardDto.get().getId());
+            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
 
                             System.out.println("This board contains lists: ");
 
-        trelloBoardDto.get().getLists().forEach(trelloList ->
+        trelloBoardDto.getLists().forEach(trelloList ->
                 System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.getIsClosed()));
 
         });
+
+        return trelloClient.getTrelloBoards();
     }
+
 
     @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard", headers = "Accept=application/json")
     public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
